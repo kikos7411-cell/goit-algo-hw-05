0cc89857487c1a10db9901e8cc8d8423fd7e54a1
kikos7411-cell <kikos7411@gmail.com>
2025-11-02 18:57:38 +0200

    calculating_total_profit.py

--- calculating_total_profit.py
+++ calculating_total_profit.py
@@ -1,12 +1,12 @@
 import re
 from typing import Callable
 
-text = ("Загальний дохід працівника складається з декількох частин:"
-        " 1000.01 як основний дохід, доповнений додатковими надходженнями"
-        " 27.45 і 324.00 доларів.")
+text = ("100.00 Загальний дохід працівника складається з декількох частин:"
+        " 1000.01 як основний дохід, доповнений додатковими надходженнями "
+                        " 27.45 і 324.00 доларів.")
 
 def generator_numbers(text: str):
-    for num in re.findall(r'\d+\.\d+', text):
+    for num in re.findall(r" "'\d+\.\d+'" ", text):
         yield float(num)
 
 def sum_profit(text: str, func: Callable):
